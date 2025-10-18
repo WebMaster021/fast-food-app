cedfe1aa7c48224b699b204a2c3bba8791b822f4
Tushar Kumar <tusharkumar20460@gmail.com>
2025-10-18 12:49:52 +0530

    Implement full auth, tab navigation

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,13 +1,29 @@
-import { View, Text } from 'react-native';
+import {View, Text, KeyboardAvoidingView, Platform, ScrollView, Dimensions} from 'react-native';
 import React from 'react';
 import {SafeAreaView} from "react-native-safe-area-context";
-import {Slot} from "expo-router";
+import {Redirect, Slot} from "expo-router";
+import {ImageBackground} from "expo-image";
+import {images} from "@/constanst";
+import {Image} from 'react-native';
+import CustomInput from "@/components/Custominput";
+import CustomButton from "@/components/Custombutton";
+import useAuthStore from "@/store/auth.store";
 
-export default function _Layout() {
+export default function AuthLayout() {
+    const {isAuthenticated} = useAuthStore();
+
+    if(isAuthenticated) return <Redirect href='/' />
     return (
-        <SafeAreaView>
-            <Text>Auth Laout</Text>
+        <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+            <ScrollView className="bg-white h-full" keyboardShouldPersistTaps = "handled">
+                <View className="w-full relative" style={{ height: Dimensions.get( 'screen' ).height / 2.25}}>
+                    <ImageBackground source={images.loginGraphic} style={{width: "100%", height: "100%"}} className=" rounded-b-lg" contentFit={"fill"}/>
+                    <Image source={images.logo} className="self-center w-48 h-48 absolute -bottom-16 z-10" />
+                </View>
+
+
             <Slot />
-        </SafeAreaView>
+            </ScrollView>
+        </KeyboardAvoidingView>
     )
 }
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,17 @@
-import {Text, View, FlatList, Pressable, TouchableOpacity, ScrollView} from "react-native";
+import {Text, View, FlatList, Pressable, TouchableOpacity, ScrollView, Button} from "react-native";
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { images, offers} from "@/constanst";
 import {Fragment} from "react";
 import {Image} from "expo-image";
 import cn from 'clsx';
 import CartButton from "@/components/CartButton";
+import useAuthStore from "@/store/auth.store";
+
 
 export default function index() {
+    const {user} = useAuthStore();
+    console.log('USER', JSON.stringify(user, null, 2))
+
     return (
         <SafeAreaView className="flex-1 bg-white">
 
